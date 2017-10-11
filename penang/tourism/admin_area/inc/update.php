a2b7d4dddda050e77dd276071a9442735d9e1894
Tan Jing Khai <hellroaming221@hotmail.com>
2017-10-11 11:07:06 +0800

    Modify Code
    Edit minor part of the codes

--- penang/tourism/admin_area/inc/update.php
+++ penang/tourism/admin_area/inc/update.php
@@ -4,19 +4,21 @@ include("includes/db.php");
 
 function getPosts()
 {
+    //expect data to get are 1 id, 1 name, 1 image and 1 description
     $posts = array();
     $posts[0] = $_POST['id'];
-    $posts[1] = $_POST['fname'];
-    $posts[2] = $_POST['lname'];
-    $posts[3] = $_POST['age'];
+    $posts[1] = $_POST['name'];
+    $posts[2] = $_FILES['image'];
+    $posts[3] = $_POST['description'];
     return $posts;
 }
 
+//Update the information
 if(isset($_POST['update']))
 {
     $data = getPosts();
     $update_Query = "UPDATE `users` 
-    SET `fname`='$data[1]',`lname`='$data[2]',`age`=$data[3] 
+    SET `name`='$data[1]',`image`='$data[2]',`desciption`=$data[3] 
     WHERE `id` = $data[0]";
     
     try{
@@ -47,21 +49,22 @@ if(isset($_POST['update']))
 
                 <tr>
                     <td align="right"><b style="font-family : Helvetica;font-size:24px;">Name:</b></td>
-                    <td><input type="text" name="name" size="60" required/></td>
+                    <td><input type="text" name="name" size="60" id="name" required/></td>
                 </tr>
 
                 <tr>
-                    <td align="right"><b style="font-family : Helvetica;font-size:24px;">Shopping Image:</b></td>
-                    <td><input type="file" name="shopping_image" required/></td>
+                    <td align="right"><b style="font-family : Helvetica;font-size:24px;">Image:</b></td>
+                    <td><input type="file" name="shopping_image" id="image"/></td>
 
                 </tr>
 
                 <tr>
                     <td align="right"><b style="font-family : Helvetica;font-size:24px;">Description:</b></td>
-                    <td><textarea type="text" rows="9" cols="60" name="desc"  ></textarea></td>
+                    <td><textarea type="text" rows="9" cols="60" name="desc"  id="desc"></textarea></td>
                 </tr>
 
-                <tr align="center">
+                <tr>
+                    <td colspan="7"><br/><input type="submit" style="background-color:#7B4A12; height:50px; font-size:25px;font-family:Helvetica;cursor:pointer;" name="update" value="Search" /></td>
                     <td colspan="7"><br/><input type="submit" style="background-color:#7B4A12; height:50px; font-size:25px;font-family:Helvetica;cursor:pointer;" name="update" value="Update" /></td>
                 </tr>
 
